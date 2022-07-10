40368999a2624beaee38ed86437d4511e330fe3c
Vitor Coelho <vitorcoelhosilvat@gmail.com>
2022-07-09 23:44:04 -0300

    Add files

---
+++ README.md
@@ -0,0 +1,21 @@
+<div align="center">
+  <img src="./assets/images/qrcode.png" width="100px">
+  <h1>Pesquisando endereço pelo CEP</h1>
+</div>
+<div>
+  <h3>Sobre o projeto</h3>
+  <p>O projeto deste repositório é um site que retorna informações sobre a rua, bairro e estado ao ser inserido um CEP no input. O projeto foi feito utilizando a <a href="https://viacep.com.br/">API ViaCEP</a>, JavaScript e Bootstrap.</a>
+
+  <h3>Informações importantes</h3>
+  <p>O projeto não está completamente finalizado ainda... 😢</p>
+
+  ### [Clique aqui para visualizar a página final do projeto](https://thenextbunny.github.io/pesquisa-cep/)
+  
+  <h3>Linguagens e frameworks utilizados</h3>
+  <ul>
+    <li>Bootstrap (versão 5.1.3)</li>
+    <li>CSS3</li>
+    <li>HTML5</li>
+    <li>JavaScript</li>
+  </ul>
+</div>
---
+++ assets/javascript/script.js
@@ -0,0 +1,26 @@
+const cep = document.querySelector('#input-cep');
+
+cep.addEventListener('keyup', () => {
+    if (cep.value.length === 8) {
+        return getCEP(cep.value);
+    }
+})
+
+function getCEP (cep) {
+    let url = `https://viacep.com.br/ws/${cep}/json`;
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.open('GET', url);
+
+    xmlHttp.onreadystatechange = () => {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            let dadosJsonText = xmlHttp.responseText;
+            let dadosJsonObj = JSON.parse(dadosJsonText);
+
+            document.querySelector('#input-endereco').value = dadosJsonObj.logradouro;
+            document.querySelector('#input-bairro').value = dadosJsonObj.bairro;
+            document.querySelector('#input-estado').value = dadosJsonObj.localidade;
+            document.querySelector('#input-uf').value = dadosJsonObj.uf;
+        }
+    }
+    xmlHttp.send();
+}
